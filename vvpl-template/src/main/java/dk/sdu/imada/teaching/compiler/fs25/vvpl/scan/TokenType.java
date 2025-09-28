2c44935e9c0f9efc812b2e583454a01465a02b91
Lucas Bergholdt Hansen <lucasbergholdt@hotmail.com>
2025-09-28 12:37:28 +0200

    TokenTypes

--- vvpl-template/src/main/java/dk/sdu/imada/teaching/compiler/fs25/vvpl/scan/TokenType.java
+++ vvpl-template/src/main/java/dk/sdu/imada/teaching/compiler/fs25/vvpl/scan/TokenType.java
@@ -7,6 +7,42 @@ package dk.sdu.imada.teaching.compiler.fs25.vvpl.scan;
 
 public enum TokenType {
     // Single-character tokens
+    LEFT_PAREN, RIGHT_PAREN, 
+    LEFT_BRACE, RIGHT_BRACE, 
+    COMMA, SEMICOLON,
+
+    // Logical operators:
+    NOT, OR, AND, 
+    TRUE, FALSE,
+
+    // Comparison:
+    EQUALS, NOT_EQUALS,
+    GREATER, GREATER_EQUAL,
+    LESS, LESS_EQUAL,
+
+    // Arithmetic:
+    PLUS, SUB, DIV, MULT,
+
+    // Declarations:
+    VAR, // "variable"
+    TYPE_DEF, // "has_type"
+    ASSIGN, // "is"
+    CAST, // "cast_to"
+    MINUS, // negative number
+    NUMBER_TYPE, STRING_TYPE, BOOL_TYPE, // variable types
+
+    // 'Functional' keywords: TODO: (idk hvad det skal hedde)
+    IF,
+    ELSE,
+    WHILE,
+    PRINT,
+    RETURN,
+    FUNCTION,
+
+    // Literals
+    IDENTIFIER, // User given names
+    NUMBER, // Some numeric value
+    STRING, // Some string
 
     // TODO add the token types
     // End-of-file
