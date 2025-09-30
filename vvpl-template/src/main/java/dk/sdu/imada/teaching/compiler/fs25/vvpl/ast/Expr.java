0071f09fc0ec24226270a5c79eb19f4e4b17007f
carlc23 <carlemildons@gmail.com>
2025-09-30 20:29:32 +0200

    Datastrukturer tilføjet

--- vvpl-template/src/main/java/dk/sdu/imada/teaching/compiler/fs25/vvpl/ast/Expr.java
+++ vvpl-template/src/main/java/dk/sdu/imada/teaching/compiler/fs25/vvpl/ast/Expr.java
@@ -1,6 +1,7 @@
 package dk.sdu.imada.teaching.compiler.fs25.vvpl.ast;
 
 import dk.sdu.imada.teaching.compiler.fs25.vvpl.ast.visitors.ExprVisitor;
+import dk.sdu.imada.teaching.compiler.fs25.vvpl.scan.Token;
 
 /**
  * @author Sandra Greiner
@@ -8,4 +9,101 @@ import dk.sdu.imada.teaching.compiler.fs25.vvpl.ast.visitors.ExprVisitor;
  */
 public abstract class Expr {
     public abstract <T> T accept(ExprVisitor<T> visitor);
+
+    
+    // --------- Nested Expr Classes -------------
+    public class Assign extends Expr {
+        public final Token ID;
+        public final Expr expr;
+
+        public Assign(Token iD, Expr expr) {
+            ID = iD;
+            this.expr = expr;
+        }
+
+        @Override
+        public <T> T accept(ExprVisitor<T> v) {
+            return v.visitAssignExpr(this);
+        }
+    }
+
+    public class Binary extends Expr {
+        public final Expr left;
+        public final Token operator;
+        public final Expr right;
+
+        public Binary(Expr left, Token operator, Expr right) {
+            this.left = left;
+            this.operator = operator;
+            this.right = right;
+        }
+
+        @Override
+        public <T> T accept(ExprVisitor<T> v) {
+            return v.visitBinaryExpr(this);
+        }
+    }
+
+    public class Logical extends Expr {
+        public final Expr left;
+        public final Token operator;
+        public final Expr right;
+
+        public Logical(Expr left, Token operator, Expr right) {
+            this.left = left;
+            this.operator = operator;
+            this.right = right;
+        }
+
+        @Override
+        public <T> T accept(ExprVisitor<T> v) {
+            return v.visitLogicalExpr(this);
+        }
+    }
+
+
+    public class Unary extends Expr {
+        public final Token operator;
+        public final Expr expr;
+
+        public Unary(Token operator, Expr expr) {
+            this.operator = operator;
+            this.expr = expr;
+        }
+
+        @Override
+        public <T> T accept(ExprVisitor<T> v) {
+            return v.visitUnaryExpr(this);
+        }
+    }
+
+    public class Literals extends Expr {
+        public final Token token;
+
+        public Literals(Token token) {
+            this.token = token;
+        }
+
+        @Override
+        public <T> T accept(ExprVisitor<T> v) {
+            return v.visitLiteralsExpr(this);
+        }
+    }
+
+
+
+
+    // C-E: what is this? Skal den ikke i Literal? Jeg har blot taget fra øvelsestimen.
+    public class Identifier extends Expr {
+        public final Token id;
+
+        public Identifier(Token id) {
+            this.id = id;
+        }
+
+        @Override
+        public <T> T accept(ExprVisitor<T> e) {
+            return e.visitIdentifierExpr(this);
+        }
+    }
 }
--- vvpl-template/src/main/java/dk/sdu/imada/teaching/compiler/fs25/vvpl/ast/Stmt.java
+++ vvpl-template/src/main/java/dk/sdu/imada/teaching/compiler/fs25/vvpl/ast/Stmt.java
@@ -1,5 +1,9 @@
 package dk.sdu.imada.teaching.compiler.fs25.vvpl.ast;
 
+import java.beans.Expression;
+import java.util.List;
+import java.util.function.Function;
+
 import dk.sdu.imada.teaching.compiler.fs25.vvpl.ast.visitors.StmtVisitor;
 
 /**
@@ -9,4 +13,83 @@ import dk.sdu.imada.teaching.compiler.fs25.vvpl.ast.visitors.StmtVisitor;
 
 public abstract class Stmt {
     public abstract <T> T accept(StmtVisitor<T> visitor);
+
+
+    // --------- Nested Stmt Classes -------------
+    public class ExprStmt extends Stmt {
+        public final Expr expr;
+
+        public ExprStmt(Expr expr) {
+            this.expr = expr;
+        }
+
+        @Override
+        public <T> T accept(StmtVisitor<T> v) {
+            return v.visitExprStmt(this);
+        }
+    }
+
+    public class IfStmt extends Stmt {
+        public final Expr cond;
+        public final Stmt thenBlock;
+        public final Stmt elseBlock;
+
+        public IfStmt(Expr cond, Stmt thenBlock, Stmt elseBlock) {
+            this.cond = cond;
+            this.thenBlock = thenBlock;
+            this.elseBlock = elseBlock;
+        }
+
+        @Override
+        public <T> T accept(StmtVisitor<T> v) {
+            return v.visitIfStmt(this);
+        }
+    }
+
+    public class WhileStmt extends Stmt {
+        public final Expr conditional;
+        public final Stmt body;
+
+        public WhileStmt(Expr conditional, Stmt body) {
+            this.conditional = conditional;
+            this.body = body;
+        }
+
+        @Override
+        public <T> T accept(StmtVisitor<T> v) {
+            return v.visitWhileStmt(this);
+        }
+    }
+
+    public class PrintStmt extends Stmt {
+        public final Expr expr;
+
+        public PrintStmt(Expr expr) {
+            this.expr = expr;
+        }
+
+        @Override
+        public <T> T accept(StmtVisitor<T> v) {
+            return v.visitPrintStmt(this);
+        }
+    }
+
+    public class BlockStmt extends Stmt {
+        public final List<Stmt> stmts;
+
+        public BlockStmt(List<Stmt> stmts) {
+            this.stmts = stmts;
+        }
+
+        @Override
+        public <T> T accept(StmtVisitor<T> v) {
+            return v.visitBlockStmt(this);
+        }
+
+        }
 }
+
+
+    
+
+
