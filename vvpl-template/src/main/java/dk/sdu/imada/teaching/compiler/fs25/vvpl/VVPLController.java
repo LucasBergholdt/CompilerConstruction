43b8aa07501d88db53a8f439531d2ba8adefbec7
Carl-Emil Dons <carlemildons@gmail.com>
2025-11-13 14:30:21 +0100

    VVPLControllerLayout

--- vvpl-template/src/main/java/dk/sdu/imada/teaching/compiler/fs25/vvpl/VVPLController.java
+++ vvpl-template/src/main/java/dk/sdu/imada/teaching/compiler/fs25/vvpl/VVPLController.java
@@ -5,6 +5,7 @@ package dk.sdu.imada.teaching.compiler.fs25.vvpl;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.LinkedList;
 import java.util.List;
 
 import dk.sdu.imada.teaching.compiler.fs25.vvpl.ast.Stmt;
@@ -12,16 +13,65 @@ import dk.sdu.imada.teaching.compiler.fs25.vvpl.interpretation.Interpreter;
 import dk.sdu.imada.teaching.compiler.fs25.vvpl.parse.Parser;
 import dk.sdu.imada.teaching.compiler.fs25.vvpl.scan.Scanner;
 import dk.sdu.imada.teaching.compiler.fs25.vvpl.scan.Token;
+import dk.sdu.imada.teaching.compiler.fs25.vvpl.semanticAnalysis.ScopeAnalyzer;
+import dk.sdu.imada.teaching.compiler.fs25.vvpl.semanticAnalysis.TypeAnalyzer;
 
 public class VVPLController {
 
-    public List<String> execute(String inputFile) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'execute'");
-    }
+	public List<String> execute(String inputFile) {
+		Scanner scanner = new Scanner(inputFile);
+		List<Token> tokens = scanner.scanTokens();
+		// Proceed ONLY if no failure.
 
-	private static boolean errorHappened = false;
+		// // print the tokens
+		// for (Token token : tokens) {
+		// System.out.println(token);
+		// }
 
+
+
+		// Parser ...
+		Parser parser = new Parser(tokens);
+		List<Stmt> stmts = parser.parse();
+		// Proceed ONLY if no failure.
+
+
+		// Semantic analysis
+		ScopeAnalyzer scopeanalyzer = new ScopeAnalyzer(stmts);
+		List<String> scopeErrors = scopeanalyzer.analyse();
+		// Proceed ONLY if no failure.
+
+
+		TypeAnalyzer typeanalyzer = new TypeAnalyzer(stmts);
+		List<String> typeErrors = typeanalyzer.analyse();
+		// Proceed ONLY if no failure.
+
+		// Interpret semantically correct programs.
+		Interpreter interpreter = new Interpreter();
+		interpreter.interpret(stmts);
+
+
+		List<String> errormessages = new LinkedList();
+		return errormessages;
+
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	/*
 	// Expects a single file that comprises a SPL program as argument
 	public static void main(String[] args) throws IOException {
 		runFile(args[0]);
@@ -31,6 +81,9 @@ public class VVPLController {
 		byte[] bytes = Files.readAllBytes(Paths.get(path));
 		run(new String(bytes));
 	}
+
+
+
 
 	private static void run(String source) {
 		Scanner scanner = new Scanner(source);
@@ -63,3 +116,5 @@ public class VVPLController {
 	}
 
 }
+
+*/
