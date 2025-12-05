804b83925fcbeb0b8834369b79ae830bdd7f54ff
Lucas Bergholdt Hansen <lucasbergholdt@hotmail.com>
2025-12-05 18:26:38 +0100

    checking for errors after parsing

--- vvpl-template/src/main/java/dk/sdu/imada/teaching/compiler/fs25/vvpl/VVPLController.java
+++ vvpl-template/src/main/java/dk/sdu/imada/teaching/compiler/fs25/vvpl/VVPLController.java
@@ -47,6 +47,10 @@ public class VVPLController {
 		Parser parser = new Parser(tokens);
 		List<Stmt> stmts = parser.parse();
 		// Proceed ONLY if no failure.
+		if (hadError) {
+			Collections.sort(errorMessages);
+			return errorMessages.stream().map(ErrorMessage::toString).toList();
+		}
 
 
 		// Semantic analysis
